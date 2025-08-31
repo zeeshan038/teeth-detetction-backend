98ae4cd71654c66c848d2c946682d05c8a874568
zeeshan038 <hankhan123623@gmail.com>
2025-08-31 14:40:01 +0500

    image upload issue fix

--- utils/multer.js
+++ utils/multer.js
@@ -1,14 +1,7 @@
 const multer = require("multer");
 const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
